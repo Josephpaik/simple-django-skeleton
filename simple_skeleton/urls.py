36c383ad6b797d2e45a7a35ce4a585229ce30563
Joseph paik <josephpaik2008@gmail.com>
2018-04-12 16:25:19 +0900

    Update urls.py
    use 'path' instead of 'url'

--- simple_skeleton/urls.py
+++ simple_skeleton/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, path
 from django.contrib import admin
 
 from simple_skeleton.apps.core import views as core_views
 
 urlpatterns = [
-    url(r'^$', core_views.home, name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+    path('', core_views.home, name='home'),
+    path('admin/', include(admin.site.urls)),
 ]
